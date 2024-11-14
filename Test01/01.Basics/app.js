184fcbce2c360d651877605d00f61365128ac329
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-14 21:13:11 +0530

    update

--- Test01/01.Basics/app.js
+++ Test01/01.Basics/app.js
@@ -1,3 +1,9 @@
 // console.log({name:"lahiru"});
-console.warn("this is warning");
+// console.warn("this is warning");
+// console.table({name:"lahiru",age:24});
+// console.clear();
 
+// Variables
+let banana;
+banana = "banana";
+console.log(banana);
