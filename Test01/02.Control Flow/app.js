31de370909941dc7180cfd7781bb70a3df37fffa
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-15 20:31:36 +0530

    Update app.js

--- Test01/02.Control Flow/app.js
+++ Test01/02.Control Flow/app.js
@@ -1,11 +1,41 @@
-let pswd = 8;
+// let pswd = 8;
 
-if (pswd === 8) {
-  console.log("welcome");
-} else if (pswd < 8) {
-  console.log("too short");
-} else if (pswd > 8) {
-  console.log("too long");
-} else {
-  console.log("try again");
+// if (pswd === 8) {
+//   console.log("welcome");
+// } else if (pswd < 8) {
+//   console.log("too short");
+// } else if (pswd > 8) {
+//   console.log("too long");
+// } else {
+//   console.log("try again");
+// }
+
+// Switch Cases
+// switch(expression){
+//   case x:
+//   //print
+//   //break;
+
+//   case y:
+//   //print
+//   //break;
+// }
+
+let fruit = "banana";
+
+switch (fruit) {
+  case "banana":
+    console.log("bana");
+    break;
+
+  case "orange":
+    console.log("org");
+    break;
+
+  case "apple":
+    console.log("apple");
+    break;
+
+  default:
+    console.log("dont knw");
 }
